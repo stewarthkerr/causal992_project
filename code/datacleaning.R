a598273d97380f5e1c0ed18ea533452cfcba84b0
Xu Jingcheng <xjc@stat.wisc.edu>
2019-11-10 20:43:53 -0600

    Construct chronic conditions and multimorbidity variables

---
+++ code/datacleaning.R
@@ -0,0 +1,51 @@
+require(tidyverse)
+
+df = read.csv('../data/data-cleaned.csv')
+
+#Combine race + ethnicity
+df <- mutate(df, RACE_ETHN =
+               case_when(
+                 RAHISPAN == "1.Hispanic" ~ "Hispanic",
+                 RARACEM == "1.White/Caucasian" ~ "NonHispWhite",
+                 RARACEM == "2.Black/African American" ~ "NonHispBlack",
+                 RARACEM == "3.Other" ~ "NonHispOther"
+                 )
+             ) %>% select(-RAHISPAN, -RARACEM)
+
+#Determine people that started with negative wealth
+df$BASELINE_POVERTY = df$H1ATOTW < 0
+
+#Calculate wealth shock
+for (i in 1:11){
+  old_wealth = paste0("H",i,"ATOTW")
+  new_wealth = paste0("H",i+1,"ATOTW")
+  equation = paste0("df$",new_wealth,"/df$",old_wealth," <= 0.25")
+  ws = eval(parse(text=equation))
+  df[paste0("WS",i+1)] = ws
+}
+
+## expand a vector of index variable (e.g. "HwATOTB") into a vector of
+## index variables for different waves (e.g. c("H1ATOTB", ...,
+## "H13TOTB"))
+expand.indeces <- function(svec) {
+    expand.single = function(str) {
+        if (str_detect(str, "w"))
+            return(map_chr(1:12,
+                           function(n) str_replace(str, "w", as.character(n))))
+        else
+            return(str)
+    }
+    unlist(map(svec, expand.single))
+}
+
+## create variables for "chronic conditions" and "multimorbidity"
+## called "RwCHRCOND" and "RwMLTMORB"
+diseases = c("RwHIBPE", "RwDIABE", "RwHEARTE", "RwSTROKE", "RwLUNGE", "RwCANCRE", "RwPSYCHE", "RwARTHRE")
+for (w in 1:12) {
+    rw.dis = select(df, one_of(str_replace(diseases, "w", as.character(w))))
+    rw.dis.ind = apply(rw.dis, 1, function(x) c(any(x == "1.Yes")))
+    rw.multi = apply(rw.dis, 1, function(x) c(sum(x == "1.Yes") >= 2))
+    eval(parse(text = paste0("df$", "R", w, "CHRCOND=rw.dis.ind")))
+    eval(parse(text = paste0("df$", "R", w, "MLTMORB=rw.multi")))
+}
+df = select(df, -one_of(expand.indeces(diseases)))
