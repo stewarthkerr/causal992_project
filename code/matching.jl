b6aff8b818bf65a94b48a49dd436090c72d7146d
shkerr <shkerr@wisc.edu>
2020-02-05 12:19:29 -0600

    Fixed up code a bit, need to figure out how to call brs_matching with generic distance calculation function

--- code/matching.jl
+++ code/matching.jl
@@ -1,6 +1,10 @@
 using JuMP, Gurobi
 using CSV, DataFrames
 
+"""
+Calculates the inverse of the covariance matrix of a matrix
+- `df` The matrix we wish to calculate covariance for, as a DataFrame
+"""
 function inv_cov(df::DataFrame)::Matrix{Float64}
     mat = Matrix(df[:, Not([:HHIDPN, :FIRST_WS, :W])])
     return inv(mat' * mat)
@@ -40,7 +44,7 @@ function pairwise_mahalanobis(trt::Int64, ctrl::Int64, S::Matrix{Float64}, df::D
     crow::Union{Nothing,Vector{Float64}} = get(datadict, (ctrl,wave)::Tuple, nothing)
 
     #If the treated/control don't have data (i.e. missing covariates), we cannot match.
-    if trow == nothing || crow == nothing
+    if trow === nothing || crow === nothing
         return LARGE_DIST
     end
 
@@ -49,6 +53,43 @@ function pairwise_mahalanobis(trt::Int64, ctrl::Int64, S::Matrix{Float64}, df::D
 
     #Multiply by inverse of covariance matrix
     return d' * S * d
+end
+
+"""
+Uses JuMP to perform balanced risk set matching 
+- `treated`  Vector of IDs of treated pool
+- `control`  Vector of IDs of match pool
+- `distance` Function used to calculate distance between treated and control
+- `numsets`  Number of matched pairs we want
+"""
+function brs_matching(treated,control,distance,numsets)#; args...)
+    #Define match Model
+    m = Model(with_optimizer(Gurobi.Optimizer, Presolve=0, OutputFlag=1, NodefileStart=0.5))
+    
+    #Below variable takes 1 if edge exists, 0 if edge does not
+    @variable(m, f[treated,control], Bin)
+
+    # Each person is in at most 1 set
+    @constraint(m, a[j in control], sum(f[i,j] for i in treated) <= 1 )
+    @constraint(m, a2[i in treated], sum(f[i,j] for j in control) <= 1 )
+
+    # Each person is not matched to themself
+    #@constraint(m, sum(x[i,i] for i in treated, i in control) <= 0)
+
+    #There are a total of S sets
+    @constraint(m, sum(f[i,j] for i in treated, j in control) >= numsets)
+
+    @objective(m, Min, sum(f[i,j] * distance(i,j,S,df,wsdict,datadict) for i in treated, j in control ))
+
+    optimize!(m)
+
+    val = objective_value(m)
+
+    val < LARGE_DIST || error("infeasible")
+
+    assignment = [ (JuMP.value(f[i,j])) for i in treated, j in control ]
+
+    return matched_sets(treated,control,assignment)
 end
 
 """
@@ -70,50 +111,27 @@ function matched_sets(treated, control, amat::Matrix)
     return set
 end
 
-function matching(treated,control,distance,numsets)
-    #Define match Model
-    m = Model(with_optimizer(Gurobi.Optimizer, Presolve=0, OutputFlag=1, NodefileStart=0.5))
-    #Below variable takes 1 if edge exists, 0 if edge does not
-    @variable(m, f[treated,control], Bin)
-
-    # Each person is in at most 1 set
-    @constraint(m, a[j in control], sum(f[i,j] for i in treated) <= 1 )
-    @constraint(m, a2[i in treated], sum(f[i,j] for j in control) <= 1 )
-
-    # Each person is not matched to themself
-    #@constraint(m, sum(x[i,i] for i in treated, i in control) <= 0)
-
-    #There are a total of S sets
-    @constraint(m, sum(f[i,j] for i in treated, j in control) >= numsets)
-
-    @objective(m, Min, sum(f[i,j]* distance(i,j,S,df,wsdict,datadict) for i in treated, j in control ))
-
-    optimize!(m)
-
-    val = objective_value(m)
-
-    val < LARGE_DIST || error("infeasible")
-
-    assignment = [ (JuMP.value(f[i,j])) for i in treated, j in control ]
-
-    return matched_sets(treated,control,assignment)
-end
-
 function main()
 
+    #LARGE_DIST is used for impossible matches
     LARGE_DIST = 1e14
-    df = CSV.read("../data/data-stacked.csv")
-    wsdict = Dict(r[:HHIDPN] => r[:FIRST_WS] for r in eachrow(unique(df[:,[:HHIDPN, :FIRST_WS]])))
+
+    #This allows us to read the data in 
+    script_location = @__DIR__
+    df = CSV.read(string(script_location,"/../data/data-stacked.csv"))
+
+    #Create dictionaries
+    wsdict = Dict(r[:HHIDPN] => r[:FIRST_WS] for r in eachrow(unique(df[:,[:HHIDPN, :FIRST_WS]])))    
     datadict = Dict( (r[:HHIDPN], r[:W])::Tuple{Int64,Int64} => Vector(r[Not([:HHIDPN, :FIRST_WS, :W])])::Vector{Float64} for r in eachrow(df) )
     treated = [ i for i in keys(wsdict) if wsdict[i] != -1 ]
     control = collect(keys(wsdict))
-    S = inv_cov(df)
 
     # count the number of matchable treated subjects
     count = sum([(t,wsdict[t]) in keys(datadict) for t in treated])
     
     #Perform the matching and write to CSV
-    match = matching(treated,control,match_dist,count)
+    S = inv_cov(df)
+    match = brs_matching(treated,control,pairwise_mahalanobis,count)
     CSV.write("../data/matched-pairs.csv", DataFrame(match), writeheader=false)
       
 end
