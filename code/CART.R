8a4b93dc1de98a76391daf105951322332cd26c6
glennss3 <61440217+glennss3@users.noreply.github.com>
2020-03-17 10:50:12 -0700

    made variables in CART factors

--- code/CART.R
+++ code/CART.R
@@ -7,10 +7,10 @@ results_final = read.csv('../data/results-final.csv')
 # Find covariates for which we have exact matching
 ### Subset results into treated and control
 treated = filter(results_final, treated == 1) %>%
-  select(-treated, -outcome) %>%
+  dplyr::select(-treated, -outcome) %>%
   arrange(pair_ID)
 control = filter(results_final, treated == 0) %>%
-  select(-treated, -outcome) %>%
+  dplyr::select(-treated, -outcome) %>%
   arrange(pair_ID)
 
 # Extract covariates that have exact matching
@@ -20,18 +20,18 @@ exact_matches = names(which((colSums(abs(treated_covariates - control_covariates
 
 # For specified covariates, find all pairs which have exact matching
 exact_covariates = c("RSMOKEV", "RAGENDER_1.Male","INITIAL_INCOME")
-exact_covariates_treated = select(treated_covariates, matches(paste(exact_covariates, collapse="|")))
-exact_covariates_control = select(control_covariates, matches(paste(exact_covariates, collapse="|")))
+exact_covariates_treated = dplyr::select(treated_covariates, matches(paste(exact_covariates, collapse="|")))
+exact_covariates_control = dplyr::select(control_covariates, matches(paste(exact_covariates, collapse="|")))
 exact_obs = which(rowSums(abs(exact_covariates_treated - exact_covariates_control)) == 0)
 
 # Now, for those pairs which have exact matching on important covariates,
 # create a CART data frame containing all exact matched covariates with outcome
 treated_CART = filter(treated, pair_ID %in% exact_obs) %>%
-  select(pair_ID, RADYEAR,
+  dplyr::select(pair_ID, RADYEAR,
     matches(paste(exact_matches, collapse="|")),
     matches(paste(exact_covariates,collapse="|")))
 control_CART = filter(control, pair_ID %in% exact_obs) %>%
-  select(pair_ID, RADYEAR,
+  dplyr::select(pair_ID, RADYEAR,
     matches(paste(exact_matches, collapse="|")),
     matches(paste(exact_covariates,collapse="|")))
 matched_pairs = inner_join(treated_CART,control_CART, by = c("pair_ID",exact_matches,exact_covariates), suffix = c(".treated",".control"))
@@ -48,9 +48,18 @@ CART_input = mutate(matched_pairs, CART_outcome = case_when(
   RADYEAR.treated < RADYEAR.control ~ abs(-1),
   RADYEAR.treated > RADYEAR.control ~ abs(1))
 ) %>%
-  select(-RADYEAR.treated, -RADYEAR.control, - pair_ID)
+  dplyr::select(-RADYEAR.treated, -RADYEAR.control, - pair_ID)
+
+###Make variables factors and only include Initial Income
+CART.input = CART_input %>%
+  dplyr::select(-INITIAL_INCOME_high.treated, -INITIAL_INCOME_low.middle.treated, 
+                -INITIAL_INCOME_middle.treated, -INITIAL_INCOME_upper.middle.treated, 
+                -INITIAL_INCOME_high.control, -INITIAL_INCOME_low.middle.control, 
+                -INITIAL_INCOME_middle.control, -INITIAL_INCOME_upper.middle.control)
+names <- c("RACE_ETHN_NonHispOther", "RMSTAT_2.Married.spouse.absent", "RMSTAT_6.Separated.divorced", "RSMOKEV", "INITIAL_INCOME", "RAGENDER_1.Male")
+CART.input[,names] <- lapply(CART.input[,names] , factor)
 
 #Build the CART
-tree = rpart(CART_outcome ~ ., data = CART_input)
+tree = rpart(CART_outcome ~ ., data = CART.input)
 rpart.plot(tree)
 
